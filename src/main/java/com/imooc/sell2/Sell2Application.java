51c6bfc0c4263b5b5b7a22dede36b318b50cdca1
ShuhaoBai <shuhao.bai@gmail.com>
2020-05-20 18:28:20 -0400

    cleaned up code

--- src/main/java/com/imooc/sell2/Sell2Application.java
+++ src/main/java/com/imooc/sell2/Sell2Application.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan(basePackages = "com.imooc.sell2.dataobject.mapper")
 public class Sell2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Sell2Application.class, args);
     }
-
 }
+
+//updated code from cleaning up
